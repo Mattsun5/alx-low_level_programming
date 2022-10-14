ef0b2b13e3ac0b4c983a00864d9914dd40a6e8c9
Mattsun5 <sundaymatt703@gmail.com>
2022-10-14 08:53:23 -0700

    alphabets upper and lower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point of program
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	char alphabet_lower;
+	char alphabet_upper;
+
+	alphabet_lower = 'a';
+	alphabet_upper = 'A';
+	while (alphabet_lower <= 'z')
+	{
+		putchar(alphabet_lower);
+		alphabet_lower++;
+	}
+	while (alphaber_upper <= "Z")
+	{
+		putchar(alphabet_upper);
+		alphabet_upper++;
+	}
+	printf("\n");
+	return (0);
+}
