61eb7c1d6d8286cc4dbb89d75a3815c69187f37d
Mattsun5 <sundaymatt703@gmail.com>
2022-10-14 10:20:00 -0700

    base 10 not using char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return - 0 when successful
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+	}
+	return (0);
+}
