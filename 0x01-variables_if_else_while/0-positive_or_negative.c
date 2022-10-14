2c66376df0ab7093b7e12c1ed307cf83626b0eb7
Mattsun5 <sundaymatt703@gmail.com>
2022-10-14 06:56:21 -0700

    if positive of negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,16 +12,19 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
-		printf(n + " is negative\n");
+		printf(n);
+		printf(" is negative\n");
 	}
 	else
 		if (n > 0)
 		{
-			printf(n + " is positive\n");
+			printf(n);
+			printf(" is positive\n");
 		}
 		else
 		{
-			printf(n + " is zero\n");
+			printf(n);
+			printf(" is zero\n");
 		}
 	return (0);
 }
