8a1850f7facc7fd96fa2d09ab37c06fdf80b8405
Mattsun5 <sundaymatt703@gmail.com>
2022-10-14 16:44:10 -0700

    no repeat numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int num, num1;
+
+	num = 0;
+	while (num < 9)
+	{
+		while (num1 < 10)
+		{
+			putchar((num % 10) + '0');
+			putchar((num1 % 10) + '0');
+
+			if ((num == 8) && (num1 == 9))
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
