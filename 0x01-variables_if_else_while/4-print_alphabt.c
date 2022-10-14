e30e43577a9371244d454c6f150740d5646785e0
Mattsun5 <sundaymatt703@gmail.com>
2022-10-14 09:05:05 -0700

    alphabets except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		if ((alpha != 'q') && (alpha != 'e'))
+		{
+			putchar(alpha);
+		}
+		alpha++;
+	}
+	return (0);
+}
