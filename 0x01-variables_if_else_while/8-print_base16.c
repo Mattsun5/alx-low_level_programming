63fd50e59d4b638355a9ed5b85edb2d2f6d434ba
Mattsun5 <sundaymatt703@gmail.com>
2022-10-14 14:53:00 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio>
+/**
+ * main - entry point
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int num;
+	char alpha;
+
+	alpha = 'a';
+	num = 0;
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+	}
+	while (alpha <= 'f')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
