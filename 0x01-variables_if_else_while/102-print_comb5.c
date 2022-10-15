894b0ea0356bf799fb10728f13070ec10d302580
Mattsun5 <sundaymatt703@gmail.com>
2022-10-14 19:23:10 -0700

    numbers to 99

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	num1 = 0;
+	while (num1 <=98)
+	{
+		num2 = num1 + 1;
+		while (num2 <= 99)
+		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 /10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 == 98 && num2 == 99)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
