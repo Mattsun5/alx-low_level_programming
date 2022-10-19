b33f0d1e230286365c714af18e2af0f4b09ded60
Mattsun5 <sundaymatt703@gmail.com>
2022-10-18 19:00:23 -0700

    advance courses

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,9 +9,11 @@ int main(void)
 	unsigned long fib1 = 0, fib2 = 1, fibsum;
 	float tot_sum;
 
-	while(1)
+	while (1)
 	{
 		fibsum = fib1 + fib2;
+		if (fibsum > 4000000)
+			break;
 		if ((fibsum % 2) == 0)
 			tot_sum += fibsum;
 
