27b33ed5c9cae6c1b18cee454c18acd0406085f5
Mattsun5 <sundaymatt703@gmail.com>
2022-10-20 19:15:26 -0700

    advn task

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - find and print largest prime
+ * of number 612852475143
+ * Return: 0
+ */
+int main(void)
+{
+	long prime = 612852475143, div;
+
+	while (div < (prime /2))
+	{
+		if ((prime % 2) == 0)
+		{
+			prime /= 2;
+			continue;
+		}
+		for (div = 3; div < (prme / 2); div += 2)
+		{
+			if ((prime % div) == 0)
+				prime /= div;
+		}
+	}
+	printf("%ld\n", prime);
+	return (0);
+}
