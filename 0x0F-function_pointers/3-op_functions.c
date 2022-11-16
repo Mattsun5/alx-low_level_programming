9672208ca6538a4ee58265d24ca67ceee714b826
Mattsun5 <sundaymatt703@gmail.com>
2022-11-16 11:15:06 -0800

    function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+/**
+ * op_add - adds two numbers
+ * @a: first num
+ * @b: second num
+ * Return: add
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts two numbers
+ * @a: first nm
+ * @b: second num
+ * Return: differenc
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two nms
+ * @a: first num
+ * @b: second num
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two nums
+ * @a: first num
+ * @b: second num
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - calculate the module of two numbers
+ * @a: first num
+ * @b: second num
+ * Return: remainder of division
+ */
+int op_nod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
