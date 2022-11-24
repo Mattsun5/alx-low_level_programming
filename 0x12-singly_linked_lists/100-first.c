71870e016475e45ec3c466c7ea03bf42479fe531
Mattsun5 <sundaymatt703@gmail.com>
2022-11-24 14:10:16 -0800

    singly linked list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,5 +6,5 @@
 void __attribute__ ((constructor)) bmain()
 {
 	printf("you're beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
+	printf(",\\nI bore my house upon my back!\n");
 }
