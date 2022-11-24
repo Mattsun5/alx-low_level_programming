9fe911f489c3eca56b6adc02d3fd03c72e87048d
Mattsun5 <sundaymatt703@gmail.com>
2022-11-24 14:13:55 -0800

    singly linked list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,13 @@
 /**
  * bmain - function executed befor main
  */
-void __attribute__ ((constructor)) bmain()
+void __attribute__ ((constructor)) hare(void);
+/**
+ * hare - prints a string before the
+ * main function is executed.
+ */
+void hare(void)
 {
-	printf("you're beat! and yet, you must allow");
-	printf(",\\nI bore my house upon my back!\n");
+	printf("you're beat! and yet, you must allow, \n"
+			"I bore my house upon my back!\n");
 }
